5a573062584c5c1acdeacdf1e404937d0268bc19
Boris <borissicsac@gmail.com>
2021-11-22 09:45:51 -0300

    renombrado para estandarizar

---
+++ generateOrders.py
@@ -0,0 +1,10 @@
+import sys
+
+""" 
+Módulo que contiene la función de generar pedidos (esta funcion es un ejemplo)
+ """
+
+def generateOrders(x):
+    return [x]
+
+sys.modules[__name__] = generateOrders
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Ejemplo de separar en módulos las funciones por si nos sirve para ir organizando
-import generatePedidos
+import generateOrders
 
 
 # Ejemplo de utilizar una función exportada por el módulo
-print(generatePedidos(3))
+print(generateOrders(3))
