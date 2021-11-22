902745cae5d180a2a4bb282de4fe79025adc21b1
gonza-jmmss <gjmmss@gmail.com>
2021-11-22 14:10:14 -0300

    Comentarios

--- generateOrders.py
+++ generateOrders.py
@@ -16,8 +16,10 @@ ordersList = []
 preparationList = []
 readyToDeliverList = []
 maxOrders = 15
-openTime = 0
-closeTime = 2100
+openTime = 2000  # Intenta representar la hora 20:00, pero luego se puede representar mejor
+closeTime = 2400  # Intenta representar la hora 24:00, pero luego se puede representar mejor
+
+# Genera una lista de diccionarios que contiene las ordenes
 
 
 def generateOrders(openTime, closeTime):
@@ -39,22 +41,29 @@ def generateOrders(openTime, closeTime):
 
 generateOrders(openTime, closeTime)
 
+# Ordena la lista de ordenes
 sortedList = sorted(ordersList, key=lambda i: i['time'])
 
 
 def OrdersStateMachine(openTime, closeTime):
+    # Recursiona sobre el horario de funcion del delivery
     for t in range(openTime, closeTime):
         # print(t)
+        # Recursiona sobre la lista de ordenes ordenadas
         for o in range(len(sortedList)):
             preparationTime = random.randint(10, 30)
+            # Si el horario coincide con la hora de la orden
             if sortedList[o]['time'] == t:
+                # Crea un nuevo item en la horden que presenta el tiempo que estará listo el pedido
                 sortedList[o]['preparedTime'] = sortedList[o]['time'] + \
                     preparationTime
                 preparationList.append(sortedList[o])
                 # print(sortedList[o])
                 del sortedList[o]
                 break
+        # Recursiona sobre la lista de pedidos en preparación
         for p in range(len(preparationList)):
+            # Si el horario coincide con la hora de preparado de la orden
             if preparationList[p]['preparedTime'] == t:
                 readyToDeliverList.append(preparationList[p])
                 # print(preparationList[p])
