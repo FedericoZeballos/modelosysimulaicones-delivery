8c910911436cfdc6550050407574d7b733e70fa9
Daniel X <danielxutuc@gmail.com>
2021-11-25 02:22:51 -0300

    restructurado del proyecto y definición de bloques en los que se debe completar

--- generateOrders.py
+++ generateOrders.py
@@ -6,23 +6,15 @@ Orden ejemplo: {idOrder:22, time:2145, x:43, y:-23, idDelivery:4}
 Consideramos el rango tanto para las cordenadas x como y entre -50 y 50
 """
 
-
 """ 
 Módulo que contiene la función de generar pedidos (esta funcion es un ejemplo)
 """
-# order = {'idOrder': 22, 'time': 2145, 'x': 43, 'y': -23, 'idDelivery': 4}
 ordersList = []
-#sortedList = []
-preparationList = []
-readyToDeliverList = []
-maxOrders = 15  # Cantidad de pedidos durante el día
-openTime = 0
-closeTime = 180
 
 # Genera una lista de diccionarios que contiene las ordenes
 
 
-def generateOrders(openTime, closeTime):
+def generateOrders(openTime, closeTime, maxOrders):
     time = random.sample(range(openTime, closeTime), maxOrders)
     for o in range(maxOrders):
         idOrder = o
@@ -36,51 +28,9 @@ def generateOrders(openTime, closeTime):
         order = {'idOrder': idOrder, 'time': time[o],
                  'x': x, 'y': y, 'idDelivery': idDelivery}
         ordersList.append(order)
+    # Ordena la lista de ordenes
+    sortedList = sorted(ordersList, key=lambda i: i['time'])
+    return sortedList
 
-
-generateOrders(openTime, closeTime)
-
-# Ordena la lista de ordenes
-sortedList = sorted(ordersList, key=lambda i: i['time'])
-
-
-def OrdersStateMachine(openTime, closeTime):
-    # Recursiona sobre el horario de funcion del delivery
-    for t in range(openTime, closeTime):
-        ##
-        print(t)
-        ##
-        # Recursiona sobre la lista de ordenes ordenadas
-        for o in range(len(sortedList)):
-            preparationTime = random.randint(10, 30)
-            # Si el horario coincide con la hora de la orden
-            if sortedList[o]['time'] == t:
-                ##
-                print(sortedList[o])
-                ##
-                # Crea un nuevo item en la horden que presenta el tiempo que estará listo el pedido
-                sortedList[o]['preparedTime'] = sortedList[o]['time'] + \
-                    preparationTime
-                # Traspaso de lista de ordenes generadas o lista de ordenes para preparar
-                preparationList.append(sortedList[o])
-                del sortedList[o]
-                break
-        # Recursiona sobre la lista de pedidos en preparación
-        for p in range(len(preparationList)):
-            # Si el horario coincide con la hora de preparado de la orden
-            if preparationList[p]['preparedTime'] == t:
-                ##
-                print(preparationList[p])
-                ##
-                # Traspaso de lista de ordenes en preparacion a lista de ordenes listas para enviar
-                readyToDeliverList.append(preparationList[p])
-                del preparationList[p]
-                break
-        sorted(readyToDeliverList, key=lambda i: i['preparedTime'])
-
-    print(readyToDeliverList)
-
-
-OrdersStateMachine(openTime, closeTime)
 
 sys.modules[__name__] = generateOrders
--- main.py
+++ main.py
@@ -1,6 +1,104 @@
-# Ejemplo de separar en módulos las funciones por si nos sirve para ir organizando
+import random
+
+# Nuestros módulos
 import generateOrders
 
 
-# Ejemplo de utilizar una función exportada por el módulo
-print(generateOrders(2000, 2400))
+# order = {'idOrder': 22, 'time': 2145, 'x': 43, 'y': -23, 'idDelivery': 4}
+
+
+# GLOBAL VARS
+openTime = 0
+closeTime = 180
+endOfDeliveryTime = closeTime + 30 # Tiempo de margen para que termine el reparto
+maxOrders = 15
+
+# ====================================
+# Variables y módulos de Órdenes (Gonza y Boris)
+# ====================================
+orderList = generateOrders(openTime, closeTime, maxOrders)
+preparationList = []
+readyToDeliverList = []
+# ====================================
+
+
+# ====================================
+# TODO (Prof): Variables y módulos de repartidores
+# ====================================
+repartidoresList = [{'id': 0, 'available': False, 'returnTime': 0}, {'id': 1, 'available': False, 'returnTime': 0}]
+repartidoresOrdersList = {
+  0: [],
+  1: [],
+}
+
+# ====================================
+# TODO (Guille y María): Variables y módulos de repartidores
+# ====================================
+deliveredOrdersList = []
+
+
+def Simular(openTime, closeTime):
+    # Recorre el tiempo
+    for t in range(openTime, closeTime):
+    	# ===================================
+    	#  ¿En el t actual, hay algún repartidor que complete su recorrido? (Daniel)
+    	# ===================================
+        for repartidorIndex in range(len(repartidoresList)):
+            # Si el horario de vuelta coincide con el tiempo actual
+            if repartidoresList[repartidorIndex]['returnTime'] == t:
+            	print("El repartidor: ", repartidoresList[repartidorIndex]['id'], "volvió")
+            	repartidoresList[repartidorIndex]['available'] = True
+    	# ===================================
+        # Recorre la lista de ordenes que entraron
+        # ===================================
+        for o in range(len(orderList)):
+            preparationTime = random.randint(10, 30)
+            # Si el horario coincide con la hora de la orden
+            if orderList[o]['time'] == t:
+                ## TIEMPO
+                print("=======  Tiempo: ",t, "=======")
+                print(orderList[o])
+                ##
+                # Crea un nuevo item en la horden que presenta el tiempo que estará listo el pedido
+                orderList[o]['preparedTime'] = orderList[o]['time'] + \
+                    preparationTime
+                # Traspaso de lista de ordenes generadas o lista de ordenes para preparar
+                preparationList.append(orderList[o])
+                del orderList[o]
+                break
+        # ===================================
+        # Recorre la lista de ordenes que están en preparación para se trasladados a reparto
+        # ===================================
+        for p in range(len(preparationList)):
+            # Si el horario coincide con la hora de preparado de la orden
+            if preparationList[p]['preparedTime'] == t:
+                ##
+                print(preparationList[p])
+                ##
+                # Traspaso de lista de ordenes en preparacion a lista de ordenes listas para enviar
+                readyToDeliverList.append(preparationList[p])
+                del preparationList[p]
+                break
+        sorted(readyToDeliverList, key=lambda i: i['preparedTime'])
+        # ===================================
+        # TODO (Prof): Recorrer la cola de repartidores disponibles y asignarle un pedido de los que estén preparados y listos.
+        #  -> y hacer el resto de cosas marcadas en el word en verde
+        # ===================================
+
+        # ===================================
+        # TODO (Guille y María): Recorrer el diccionario de repartidores que tienen al menos un pedido para llevar y generar recorrido
+        # -> funciones sugeridas
+        #        getRoute(pedidosRepartidor) # retorna la ruta
+        # -> y hacer el resto de cosas marcadas en el word en cyan
+    	#            campos a utilizar para marcar los pedidos: travelStartTime y arrivalTime
+        # ===================================
+
+    # ===================================
+    # Acá se retornarán resultados una vez termine el tiempo:
+    # ===================================    
+    print(repartidoresList)
+    # FIN
+
+# EJECUTAMOS LA SIMULACION
+# DEJAMOS UN MARGEN DE TIEMPO PARA QUE REPARTA LOS ÚLTIMOS PEDIDOS EN BASE A LOS RETRASOS POR PREPARACIÓN.
+Simular(openTime, endOfDeliveryTime)
